7482a44050b64d9cb5ed4540ef0519993291ad29
Papillon <75226598+con-dog@users.noreply.github.com>
2025-01-14 20:57:37 +1300

    update audio constants

--- audio/constants.h
+++ audio/constants.h
@@ -2,16 +2,47 @@
 #define AUDIO_CONSTANTS_H
 
 #define C0 16.35f
-#define C1 32.7f
-#define C2 65.41f
-#define C3 130.81f
-#define C4 261.63f
-#define C5 532.25f
-#define C6 1046.5f
-#define C7 2093.0f
-#define C8 4186.0f
+#define C1 32.703f
+#define C2 65.406f
+#define C3 130.813f
+#define C4 261.626f
+#define C5 532.251f
+#define C6 1046.502f
+#define C7 2093.005f
+#define C8 4186.009f
+#define C9 8372.018f
 
-#define D4 277.18f
-#define E4 329.63f
+#define CSH0 17.324f
+#define CSH1 34.648f
+#define CSH2 69.296f
+#define CSH3 138.591f
+#define CSH4 277.183f
+#define CSH5 554.365f
+#define CSH6 1108.731f
+#define CSH7 2217.461f
+#define CSH8 4434.922f
+#define CSH9 8869.844f
+
+#define DFLT0 CSH0
+#define DFLT1 CSH1
+#define DFLT2 CSH2
+#define DFLT3 CSH3
+#define DFLT4 CSH4
+#define DFLT5 CSH5
+#define DFLT6 CSH6
+#define DFLT7 CSH7
+#define DFLT8 CSH8
+#define DFLT9 CSH9
+
+#define D0 18.354f
+#define D1 36.708f
+#define D2 73.416f
+#define D3 146.832f
+#define D4 293.665f
+#define D5 587.330f
+#define D6 1174.659f
+#define D7 2349.318f
+#define D8 4698.636f
+#define D9 9397.272f
 
 #endif
