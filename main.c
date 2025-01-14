880a123d5803ac53fc97a20ea7c2836574122d48
Papillon <75226598+con-dog@users.noreply.github.com>
2025-01-14 16:33:07 +1300

    ...

--- main.c
+++ main.c
@@ -9,28 +9,31 @@ Byte sinusoidal_sample(DWord sample_no, Frequency note)
 
 int main(void)
 {
-  WavHeader header = {0}; // Initialize all to 0 first
+  WavHeader header = {0};
 
+  /* ----------------
+   * Header init
+   * --------------*/
   // RIFF header
-  header.riff.file_type_bloc_id = *(DWord *)"RIFF"; // This handles endianness correctly
+  header.riff.file_type_bloc_id = *(DWord *)"RIFF";
   header.riff.file_format_id = *(DWord *)"WAVE";
-
   // Format chunk
   header.format.format_bloc_id = *(DWord *)"fmt ";
-  header.format.bloc_size = 16;    // Size of format chunk
-  header.format.audio_format = 1;  // PCM
-  header.format.nbr_channels = 2;  // Stereo
-  header.format.frequency = 44100; // 44.1kHz
+  header.format.bloc_size = 16;
+  header.format.audio_format = 1;
+  header.format.nbr_channels = 2;
+  header.format.frequency = 44100;
   header.format.bits_per_sample = 8;
   header.format.byte_per_bloc = (header.format.nbr_channels * header.format.bits_per_sample) / 8;
   header.format.byte_per_sec = header.format.frequency * header.format.byte_per_bloc;
-
   // Data chunk
   header.sample.data_bloc_id = *(DWord *)"data";
 
-  // Calculate samples
+  /* ----------------
+   * Calculate samples
+   * --------------*/
   Seconds t = 1;
-  int num_samples = SAMPLE_RATE_CD * t * 2; // 2 channels
+  int num_samples = SAMPLE_RATE_CD * t * header.format.nbr_channels;
   Byte *stereo_samples = malloc(num_samples);
   if (!stereo_samples)
   {
@@ -45,10 +48,17 @@ int main(void)
     stereo_samples[i + 1] = sinusoidal_sample(sample_num, C4);
   }
 
+  /* ----------------
+   * Update file size
+   * --------------*/
+
   // Set sizes
   header.sample.data_size = num_samples;
-  header.riff.file_size = sizeof(WavHeader) - 8 + num_samples; // -8 for RIFF header
+  header.riff.file_size = sizeof(WavHeader) - 8 + num_samples;
 
+  /* ----------------
+   * Write to file
+   * --------------*/
   FILE *fp = fopen("out.wav", "wb");
   if (fp == NULL)
   {
@@ -62,5 +72,6 @@ int main(void)
 
   free(stereo_samples);
   fclose(fp);
+
   return EXIT_SUCCESS;
 }
